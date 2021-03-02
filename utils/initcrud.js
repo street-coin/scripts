a29c8feaf160d6f9680a203562b8bf9fbc345ede
luctst <lucas.tostee@gmail.com>
2021-03-02 21:28:56 +0100

    Write logic for fetch models

--- utils/initcrud.js
+++ utils/initcrud.js
@@ -1,15 +1,23 @@
-const { createWriteStream, mkdir } = require("fs");
-const { request } = require('https');
-const axios = require('axios');
+const { promises} = require("fs");
+const { get } = require('https');
 
-module.exports = function (data) {
-    const modelStream = createWriteStream(`${process.cwd()}/tmp/${data.service}.js`);
-    request('https://api.github.com/repos/street-coin/api/contents/models', function (models) {
-        models.on('data', d => console.log(d));
-
-        mkdir(`${process.cwd()}/tmp`, { recursive: true }, (err) => {
-            if (err) throw err;
-            process.exit(0);
+module.exports = async function (data) {
+    get(
+        {
+            host: 'api.github.com',
+            path: `/repos/street-coin/api/contents/models/${data.service}.js`,
+            method: 'GET',
+            headers: { 'User-Agent': 'street-coin' }
+        }, function (model) {
+            let modelData = '';
+            
+            model.setEncoding('utf8');
+            model
+            .on('data', d => modelData += d)
+            .on('end', async function () {
+                await promises.mkdir(`${process.cwd()}/tmp`, { recursive: true });
+                const modelContent = Buffer.from(JSON.parse(modelData).content, 'base64').toString('utf-8');
+                await promises.writeFile(`${process.cwd()}/tmp/${data.service}.js`, modelContent);
+            });
         });
-    });
 };
