d9501b71de1613ea913ab8020ffc191b0b4e77f2
luctst <lucas.tostee@gmail.com>
2021-03-09 10:37:01 +0100

    Add fn who populate schemas

--- commands/models/index.js
+++ commands/models/index.js
@@ -1,20 +1,25 @@
+#!/usr/bin/env node
 const { promises, createWriteStream } = require('fs');
-const { get } = require('https');
 
+const axios = require('axios');
 const { prompt } = require('inquirer');
 const chalk = require('chalk');
 
+async function populateSchemas() {
+    const modelsData = await axios.get(`https://api.github.com/repos/street-coin/api/contents/models`);
+    
+    await Promise.all(modelsData.data.map(async function (model) {
+        const t = await axios.get(model.download_url, { responseType: 'stream' });
+        t.data.pipe(createWriteStream(`${__dirname}/schemas/${model.name}`));
+    }));
+
+    console.log(chalk`{cyan It's done your Schemas are created you can now manipulate your database.}`);
+    process.exit(0);
+}
+
 module.exports = async function () {
     if (!(await promises.readdir(`${__dirname}/schemas`)).length) {
-        return get(
-            {
-                host: 'api.github.com',
-                path: `/repos/street-coin/api/contents/models`,
-                method: 'GET',
-                headers: { 'User-Agent': 'street-coin' }
-            }, function (models) {
-            }
-        );
+        return await populateSchemas();
     }
 
     const awsr = prompt([
@@ -25,5 +30,7 @@ module.exports = async function () {
         }
     ]);
 
-    if (awsr.fetch) {}
+    if (awsr.fetch) {
+        return await populateSchemas();
+    }
 };
