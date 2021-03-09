ea12838dc6d2cc9fbed1fbd7fa4458bcb56a9b28
luctst <lucas.tostee@gmail.com>
2021-03-09 10:12:33 +0100

    Test

--- commands/bdd/crud/association.js
+++ commands/bdd/crud/association.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
+const AssosModel = require('../../models/schemas/association');
+
+async function add(userData) {}
+
 module.exports = async function (userData) {
-    console.log(userData);
-    await mongoose.connection.close();
 }
