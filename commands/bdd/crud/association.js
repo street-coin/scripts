a0417bee3216a8f241a0bdca9c0ba634f7d4c87e
luctst <lucas.tostee@gmail.com>
2021-04-12 00:21:00 +0200

    Remove useless fn

--- commands/bdd/crud/association.js
+++ commands/bdd/crud/association.js
@@ -1,8 +1,3 @@
 const mongoose = require('mongoose');
 
 const AssosModel = require('../../models/schemas/association');
-
-async function add(userData) {}
-
-module.exports = async function (userData) {
-}
